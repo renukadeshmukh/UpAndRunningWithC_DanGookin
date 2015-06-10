bede9ca6161f5f2466521ae40a55f21a41043c42
Renuka Deshmukh <renud1988@gmail.com>
2015-06-10 12:07:18 -0700

    Printing % in printf

--- 02-12_value3.c
+++ 02-12_value3.c
@@ -2,6 +2,7 @@
 
 int main()
 {
+    /* Exploring %c and %s in the printf function */
 	printf("You are a %c %s\n", 'C' , "programmer");
 
 	return(0);
---
+++ 02-12_value4.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main()
+{
+    /* If you want to print %, add two percent signs*/
+	printf("I got %d%% on my C exam!\n",98);
+
+	return(0);
+}
+
