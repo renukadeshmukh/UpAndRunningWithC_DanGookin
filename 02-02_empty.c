065d8eae544e55d3ed5d997639a3eb3d508778fc
Renuka Deshmukh <renud1988@gmail.com>
2015-06-10 11:56:17 -0700

    Exploring puts and printf statements

---
+++ 02-02_empty.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main()
+{
+	puts("Thi is a computer!");
+	puts("This is my first attemp at using the puts function.");
+
+	return(0);
+}
+
---
+++ 02-06_printf.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main()
+{
+	printf("This is the way the world ends.\n");
+	printf("Not with a bang but a whimper.\n");
+
+	printf("I am %d years old.\n", 26);
+	printf("I have %d brothers and %d sisters!", 2 ,2);
+
+	return(0);
+}
+
