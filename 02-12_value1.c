db3aa57b0352328b3a958c1860594d144e638fbe
Renuka Deshmukh <renud1988@gmail.com>
2015-06-10 12:01:39 -0700

    Printf function to perform arithmetic functions

---
+++ 02-12_value1.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main()
+{
+	printf("Here is a value: 27\n");
+
+	printf("Here is the value : %d\n", 27);
+	printf("Here are the values : %d, %d and %d\n", 25,26,27);
+
+	return(0);
+}
+
---
+++ 02-12_value2.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main()
+{
+    /*
+    Printf function to perform arithmetic functions
+    */
+	printf("Everyone knows that 278+956=%d\n",278*956);
+
+	return(0);
+}
+
