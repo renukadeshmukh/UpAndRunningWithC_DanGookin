21dd49f18a097660924343118102bab3684bdb16
Renuka Deshmukh <renud1988@gmail.com>
2015-06-10 12:21:04 -0700

    Introducing variables

---
+++ 02-13_variable1.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main()
+{
+	int age;
+
+	age = 34;
+	printf("The C language is over %d years old!\n",age);
+
+	age = 50;
+	printf("This course's author is over %d years old\n" , age);
+
+	return(0);
+}
+
