126c31354c1d7fc058d897664b30c938b80638c7
Renuka Deshmukh <renud1988@gmail.com>
2015-06-10 12:45:56 -0700

    Introducing float variables

---
+++ 02-13_variable4.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main()
+{
+	float pi;
+
+	pi = 22.00 / 7.0;
+	printf("The ancients calculated PI as %f.\n",pi);
+
+	int pi1 = 22.0/7.0;
+	printf("The value of pi is %d\n", pi1);
+
+	return(0);
+}
+
