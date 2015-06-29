b4a1c16d3770eed1dd5ee072a1e11b207d8abe77
Renuka Deshmukh <renud1988@gmail.com>
2015-06-29 16:01:16 -0700

    Exploring strings and more I/O

---
+++ 02-15_scanf1.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main()
+{
+	int x;
+
+	printf("Type an integer: ");
+	scanf("%d",&x);
+	printf("Integer %d\n",x);
+
+	float y;
+
+	printf("\nType a floating ponit value : ");
+	scanf("%f", &y);
+	printf("\nFloating point value is %f", y);
+
+	return(0);
+}
+
