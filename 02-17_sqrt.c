e3ba8d4dec51a79b3d6e1d3142a6a39b163d9ace
Renuka Deshmukh <renud1988@gmail.com>
2015-06-29 16:33:24 -0700

    Making more calculations

---
+++ 02-17_sqrt.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <math.h>
+
+int main()
+{
+    float r;
+
+    r = sqrt(2.0);
+
+	printf("The square root of 2.0 is %f\n",r);
+    printf("The square root of 2 is %f\n",sqrt(2));
+
+    printf("The square root of 4 is %f\n",sqrt(4));
+    printf("The square root of 4.0 is %f\n",sqrt(4.0));
+
+	return(0);
+}
