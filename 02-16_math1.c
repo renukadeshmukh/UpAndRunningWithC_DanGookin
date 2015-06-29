7fe338b4f7045cc7ddf49008778d60dd52d56f8f
Renuka Deshmukh <renud1988@gmail.com>
2015-06-29 16:25:31 -0700

    Working with Math

---
+++ 02-16_math1.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main()
+{
+	int a;
+	int b = 5;
+
+	printf("Input an integer: ");
+	scanf("%d",&a);
+	printf("%d + %d = %d\n",a,b,a+b);
+
+    printf("%d - %d = %d\n",a,b,a-b);
+
+	printf("%d * %d = %d\n",a,b,a*b);
+
+    printf("%d / %d = %d\n",a,b,a/b);
+
+	return(0);
+}
+
---
+++ 02-16_math4.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main()
+{
+	int a;
+
+	a = 25 / 5 * 2 + 3;
+	printf("The answer is %d\n",a);
+
+	a = 25 / 5 * (2 + 3);
+	printf("The answer is %d\n",a);
+
+	a = 25 / (5 * (2 + 3));
+	printf("The answer is %d\n",a);
+
+
+
+	return(0);
+}
+
