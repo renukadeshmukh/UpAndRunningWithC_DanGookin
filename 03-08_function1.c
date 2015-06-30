2afdb63a56bc94c69a5a56df5a34d4efdaae05bb
Renuka Deshmukh <renud1988@gmail.com>
2015-06-30 10:12:33 -0700

    Adding a function

---
+++ 03-08_function1.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+void blorf(void);
+
+int main()
+{
+	puts("The main() function always runs first");
+	blorf();
+	blorf();
+	puts("Thanks, blorf()");
+
+	return(0);
+}
+
+void blorf(void)
+{
+	puts("The blorf() function runs when it's called");
+}
+
