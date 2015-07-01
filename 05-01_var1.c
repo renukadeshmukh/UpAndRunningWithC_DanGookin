76a51e2078a2f760d385ac6fc47fd7adb384f1a7
Renuka Deshmukh <renud1988@gmail.com>
2015-07-01 14:48:20 -0700

    Understanding variables

---
+++ 05-01_var1.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main()
+{
+	int a = 45;
+
+	printf("The value of a is %d\n",a);
+
+	return(0);
+}
+
---
+++ 05-01_var2.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main()
+{
+	long a;
+
+	printf("An long int variable occupies %lu bytes of storage\n",sizeof(a));
+
+	return(0);
+}
+
