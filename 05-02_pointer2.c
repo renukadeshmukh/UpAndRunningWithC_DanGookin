51d604d5520eba89883ac26d50db09aff318670a
Renuka Deshmukh <renud1988@gmail.com>
2015-07-02 11:18:59 -0700

    Challenge: Working with pointers

--- 05-02_pointer2.c
+++ 05-02_pointer2.c
@@ -7,12 +7,27 @@ int main()
 
 	pokey = 987;
 	p = &pokey;
-	
+
 	printf("The address of `pokey` is %p\n",&pokey);
 	printf("The contents of `pokey` are %d\n",pokey);
 
 	printf("The address of `pokey` is %p\n",p);
 	printf("The contents of `pokey` are %d\n",*p);
+
+
+    char cpokey;
+	char *cp;
+
+	cpokey = 'a';
+	cp = &cpokey;
+
+	printf("The address of `cpokey` is %p\n",&cpokey);
+	printf("The contents of `cpokey` are %c\n",cpokey);
+
+	printf("The address of `cpokey` is %p\n",cp);
+	printf("The contents of `cpokey` are %c\n",*cp);
+
+
 
 	return(0);
 }
